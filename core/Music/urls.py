76d242553c923e8f6d8e35a06b2aaf537b0ca811
matinmat8 <matinmat8.mat@gmail.com>
2024-06-19 01:22:27 +0330

    testing if the task working

--- core/Music/urls.py
+++ core/Music/urls.py
@@ -1,6 +1,10 @@
 from django.conf import settings
 from django.conf.urls.static import static
+from django.urls import path
+from .views import detail_getter
+
+app_name = 'music'
 
 urlpatterns = [
-    # ... other URL patterns ...
+    path('detail_getter/<int:id>/', detail_getter),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- core/Music/views.py
+++ core/Music/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from .tasks import increment_post_view_count
+from django.views.generic import DetailView
+from .models import Music
 
-# Create your views here.
+
+def detail_getter(request, id):
+    music = Music.objects.get(ID=id)
+    result = increment_post_view_count.delay(music_id=id)
+    return JsonResponse({'task_id': result.task_id})
