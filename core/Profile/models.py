8336c8914acdf360f58218a96a978b9252b9cb80
matinmat8 <matinmat8.mat@gmail.com>
2024-06-22 20:24:56 +0330

    Fixing circular importing error

--- core/Profile/models.py
+++ core/Profile/models.py
@@ -1,15 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.db.models.signals import pre_save
-from django.dispatch import receiver
-
-from Music.models import Music
 
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    recently_played = models.ManyToManyField(Music, "musics", blank=True, null=True)
-    favorite_list = models.ManyToManyField(Music, "favorite_musics", blank=True, null=True)
+    recently_played = models.ManyToManyField('Music.Music', "musics", blank=True, null=True)
+    favorite_list = models.ManyToManyField('Music.Music', "favorite_musics", blank=True, null=True)
     introduction = models.TextField(blank=True, null=True)
     # Other profile stuffs will come here....
 
